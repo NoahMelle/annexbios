c2df22fad41acd61844a4a97474ad468dd3ab523
NoahMelle <noahmkamphuisen@gmail.com>
2024-09-03 22:13:56 +0200

    routing fixes

--- core/router.php
+++ core/router.php
@@ -1,21 +1,34 @@
 <?php
-
-$view = '';
-if (!empty($_GET['view'])) {
+$view = [];
+if (isset($_GET['view'])) {
     $view = mes($_GET['view']);
-    $view = str_replace("/", "", $view);
+    $view = explode("/", $view);
+} else {
+    $view = [''];
 }
 
-switch ($view) {
+$page = '';
+$style = '';
+$js = '';
+$isApiCall = false;
+
+switch ($view[0]) {
     case '':
         $template = "home";
         $model = "home";
         break;
-    case 'products':
-        $template = "products";
-        $model = "products";
-        break;
+    case 'api':
+        $isApiCall = true;
 
+        switch ($view[2]) {
+            case 'playingMovies':
+                $page = "v1/movies/getPlayingMovies.php";
+                break;
+            case 'reservePlace':
+                $page = "v1/movies/reservePlace.php";
+                break;
+        }
+        break;
     default:
         $template = "404";
         $model = "404";
