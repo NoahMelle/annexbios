3cf2bafa033a00e566c10c9b6e49b33d997d495e
PepijnBullens <pepijnpersoonlijk@gmail.com>
2024-09-05 16:08:49 +0200

    made location on home page dynamic

--- model/home.php
+++ model/home.php
@@ -2,39 +2,21 @@
     'page_title' => "AnnexBios",
     'styles' => ['homepage.css'],
     'js' => ['load_movies.js', 'load_news.js'],
-    'locations' => array(
-        array(
-            'city' => 'Leerdam',
-            'address' => 'Techniekweg 6',
-            'postal_code' => '4143 HV',
-            'url' => 'https://annexbiosleerdam.gluwebsite.nl/'
-        ),
-        array(
-            'city' => 'Maarssen',
-            'address' => 'Industrieweg 54 68',
-            'postal_code' => '3606 AS',
-            'url' => 'https://annexbiosmaarssen.gluwebsite.nl/'
-        ),
-        array(
-            'city' => 'Leidsche Rijn',
-            'address' => 'Berlijnplein 101',
-            'postal_code' => '3541 CM',
-            'url' => 'https://annexbiosleidscherijn.gluwebsite.nl/'
-        ),
-        array(
-            'city' => 'Montfoort',
-            'address' => 'Vlasakker 39',
-            'postal_code' => '3417 DB',
-            'url' => 'https://annexbiosmontfoort.gluwebsite.nl/'
-        ),
-        array(
-            'city' => 'Breukelen',
-            'address' => 'De Corridor 10 E',
-            'postal_code' => '3621 ZB',
-            'url' => 'https://annexbiosbreukelen.gluwebsite.nl/'
-        ),
-    ),
+    'locations' => [],
     'skeleton-loader-amt' => range(1, 10),
     'stars' => range(1, 5),
     'news-items' => range(1, 10),
 ];
+
+$stmt = $con->prepare("SELECT image_path, city, address, postal_code, website_link FROM location_data");
+$stmt->execute();
+$result = $stmt->get_result();
+while ($row = $result->fetch_assoc()) {
+    $data["locations"][] = [
+        'image_path' => !empty($row['image_path']) ? $row['image_path'] : 'default.png',
+        'city' => $row['city'],
+        'address' => $row['address'],
+        'postal_code' => $row['postal_code'],
+        'website_link' => $row['website_link'],
+    ];
+}
