07b5663038160005c568908f3b7d0d2071a04ab5
= <noahmkamphuisen@gmail.com>
2024-09-09 18:37:38 +0200

    Front-end add user form working

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,4 +1,79 @@
 const newUserForm = document.getElementById("new-user-form");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const addUserButton = document.getElementById("add-user-submit");
+const permissionCheckboxes = document.querySelectorAll(".permission-checkbox");
+const permissionsError = document.getElementById("permissions-error");
+let containsErrors = true;
+
+const onPasswordChange = (event) => {
+  const password = event.target.value;
+  const passwordError = document.getElementById("password-error");
+  if (validatePassword(password)) {
+    // if password valid
+    passwordError.textContent = "";
+    checkFullValidation();
+  }
+  else if (password.length === 0) {
+    // if password is empty, still not valid
+    containsErrors = true;
+    passwordError.textContent = "";
+  } else {
+    // if password invalid
+    containsErrors = true;
+    passwordError.textContent =
+      "Wachtwoord moet minimaal 8 karakters lang zijn en minimaal 1 letter en 1 cijfer bevatten.";
+  }
+}
+
+const onCheckboxChange = (event) => {
+  const checkboxesChecked = Array.from(permissionCheckboxes).some(
+    (checkbox) => checkbox.checked
+  );
+  if (checkboxesChecked) {
+    permissionsError.textContent = "";
+    checkFullValidation();
+  } else {
+    permissionsError.textContent = "Selecteer minimaal één permissie.";
+    containsErrors = true;
+  }
+}
+
+const onUsernameChange = (event) => {
+  const username = event.target.value;
+  const usernameError = document.getElementById("username-error");
+  if (validateUsername(username)) {
+    // if username valid
+    usernameError.textContent = "";
+    checkFullValidation();
+  } else if (username.length === 0) {
+    // if username is empty, still not valid
+    containsErrors = true;
+    usernameError.textContent = "";
+  } else {
+    // if username invalid
+    containsErrors = true;
+    usernameError.textContent =
+    "Gebruikersnaam moet tussen de 3 en 255 karakters lang zijn en mag alleen letters, cijfers en underscores bevatten.";
+  }
+};
+
+function checkFullValidation() {
+  if (validateUsername(usernameInput.value) && validatePassword(passwordInput.value)) {
+    containsErrors = false;
+  } else {
+    containsErrors = true;
+  }
+  addUserButton.disabled = containsErrors
+  console.log(containsErrors);
+}
+
+passwordInput.addEventListener("input", onPasswordChange);
+usernameInput.addEventListener("input", onUsernameChange);
+permissionCheckboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", onCheckboxChange);
+});
+checkFullValidation();
 
 function generateRandomPassword(element) {
   const randomPassword = generateCustomPassword(32);
@@ -31,4 +106,12 @@ function generateCustomPassword(length) {
   }
 
   return password;
+}
+
+function validateUsername(username) {
+  return /^[a-zA-Z0-9_]{3,255}$/.test(username);
+}
+
+function validatePassword(password) {
+  return /^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z0-9!"#$%&'()*+,-./]{8,}$/.test(password);
 }
