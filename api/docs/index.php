752fffd6328eb5b8d06a39177d6cc52a41ec2769
PepijnBullens <pepijnpersoonlijk@gmail.com>
2024-09-12 10:55:22 +0200

    removed var_dump from docs

--- api/docs/index.php
+++ api/docs/index.php
@@ -1,11 +1,10 @@
 <?php
   require_once '../../core/db_connect.php';
-
-  var_dump($env);
 ?>
 
 <!doctype html>
 <html>
+
 <head>
     <title>AnnexBios API Documentatie</title>
     <meta charset="UTF-8" />
@@ -14,14 +13,16 @@
     <link rel="stylesheet" href="https://cdn.zudoku.dev/latest/style.css" crossorigin />
 
 </head>
-<body>
-<!--<div data-api-url="https://annexbios.nickvz.nl/api-config.yaml"></div>-->
-<div data-api-url="<?= $env['BASEURL'] ?>/api/api-config.yaml"></div>
 
-<style>
-    body > div > header > div > div > div.flex > a > div > span {
+<body>
+    <!--<div data-api-url="https://annexbios.nickvz.nl/api-config.yaml"></div>-->
+    <div data-api-url="<?= $env['BASEURL'] ?>/api/api-config.yaml"></div>
+
+    <style>
+    body>div>header>div>div>div.flex>a>div>span {
         display: none;
     }
-</style>
+    </style>
 </body>
+
 </html>
